c5a0a020975483ffb5091fd06bdbef7f22c75308
skipperokk <Kh9yK$Rf>
2020-04-25 20:53:00 +0300

    Lesson 10 create Computer, use Factory method

--- src/main/java/ru/springcourse/MusicPlayer.java
+++ src/main/java/ru/springcourse/MusicPlayer.java
@@ -1,22 +1,28 @@
 package ru.springcourse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MusicPlayer {
 
-//    private List<Music> musicList = new ArrayList<>();
+    //    private List<Music> musicList = new ArrayList<>();
 //
 //    public void setMusicList(List<Music> musicList) {
 //        this.musicList = musicList;
 //    }
 
-    private Music music;
+    private ClassicalMusic classicalMusic;
+    private RockMusic rockMusic;
 
-    // IoC
-    public MusicPlayer(Music music) {
-        this.music = music;
+    @Autowired
+    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
+        this.classicalMusic = classicalMusic;
+        this.rockMusic = rockMusic;
     }
 
-    public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+    public String playMusic() {
+        return "Playing: " + classicalMusic.getSong();
     }
 
 //    private String name;
@@ -37,7 +43,6 @@ public class MusicPlayer {
 //    public void setVolume(int volume) {
 //        this.volume = volume;
 //    }
-
 
 
 //    public MusicPlayer() {
