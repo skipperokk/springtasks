54cefe768ccf81b7d7472964269289297f03afff
skipperokk <Kh9yK$Rf>
2020-04-25 13:52:07 +0300

    Lesson 3 (use Dependency Injection with XML)

--- src/main/java/ru/springcourse/TestSpring.java
+++ src/main/java/ru/springcourse/TestSpring.java
@@ -9,10 +9,13 @@ public class TestSpring {
         );
 
         // получаем bean
-        Music music = context.getBean("musicBean", Music.class);
+//        Music music = context.getBean("musicBean", Music.class);
+//
+//        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = new MusicPlayer(music);
-
+        // передает в качестве бина id musicPlayer
+        // Dependency Injection (не внедряем зависимость вручную, а делаем это через Spring)
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
 
         context.close();
