8574f97d09ae7353aafa03332173a9cd730f7cf5
skipperokk <Kh9yK$Rf>
2020-04-25 16:33:56 +0300

    Lesson 9 create beans with Annotation (@Component)

--- src/main/java/ru/springcourse/ClassicalMusic.java
+++ src/main/java/ru/springcourse/ClassicalMusic.java
@@ -1,21 +1,11 @@
 package ru.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
 
     private ClassicalMusic() {
-
-    }
-
-    public static ClassicalMusic getClassicalMusic() {
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit() {
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroy() {
-        System.out.println("Doing my destruction");
     }
 
     @Override
@@ -23,3 +13,16 @@ public class ClassicalMusic implements Music {
         return "Hungarian Rhapsody";
     }
 }
+
+//    public static ClassicalMusic getClassicalMusic() {
+//        return new ClassicalMusic();
+//    }
+//
+//    public void doMyInit() {
+//        System.out.println("Doing my initialization");
+//    }
+//
+//    public void doMyDestroy() {
+//        System.out.println("Doing my destruction");
+//    }
+
--- src/main/java/ru/springcourse/TestSpring.java
+++ src/main/java/ru/springcourse/TestSpring.java
@@ -8,22 +8,36 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
-
-        System.out.println(classicalMusic.getSong());
+        Music music = context.getBean("rockMusic", Music.class);
 
 
+        MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        musicPlayer.playMusic();
+
+        Music music2 = context.getBean("classicalMusic", Music.class);
+
+        MusicPlayer classicalMusicPlayer = new MusicPlayer(music2);
+
+        classicalMusicPlayer.playMusic();
+
+        context.close();
+    }
+}
 
 
+//        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+//
+//        System.out.println(classicalMusic.getSong());
 
 
-        // получаем bean
+// получаем bean
 //        Music music = context.getBean("musicBean", Music.class);
 //
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        // передает в качестве бина id musicPlayer
-        // Dependency Injection (не внедряем зависимость вручную, а делаем это через Spring)
+// передает в качестве бина id musicPlayer
+// Dependency Injection (не внедряем зависимость вручную, а делаем это через Spring)
 //        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 //        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
 //        // musicPlayer.playMusicList();
@@ -39,8 +53,3 @@ public class TestSpring {
 //
 //        System.out.println(firstMusicPlayer.getVolume());
 //        System.out.println(secondMusicPlayer.getVolume());
-
-
-        context.close();
-    }
-}
